c9542619fa17fa40b7053ac908ef54937f766ae4
Nghia Tran <119276144+nghiatm341@users.noreply.github.com>
2025-01-07 00:13:19 +0700

    Checking nonsense settings

--- common.js
+++ common.js
@@ -2,3 +2,16 @@ const allForms = document.querySelectorAll("form")
 allForms.forEach(form => {
     form.addEventListener("submit", e => {e.preventDefault()})
 })
+
+const alertPlaceholder = document.getElementById('liveAlertPlaceholder')
+const appendAlert = (message, type) => {
+  const wrapper = document.createElement('div')
+  wrapper.innerHTML = [
+    `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+    `   <div>${message}</div>`,
+    '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+    '</div>'
+  ].join('')
+
+  alertPlaceholder.append(wrapper)
+}
