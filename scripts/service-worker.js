7ac97ad6ef57ab829e447da75a599260c56e520b
Nghia Tran <119276144+nghiatm341@users.noreply.github.com>
2025-01-10 00:50:56 +0700

    update

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -6,7 +6,6 @@ let urlsToCache = [
     "/index.html",
     "/exam.html",
     "/settings.html",
-    "/images",
     "/images/icon-48x48.png", 
     "/scripts/common.js", 
     "/scripts/data.js", 
@@ -31,15 +30,19 @@ self.addEventListener("install", (event) => {
 
 // Whenever a resource is requested, return if its cached else fetch the resource from server.
 self.addEventListener("fetch", (event) => {
+    // Cache-First Strategy
+    console.log("fetch event")
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            if (response) {
-                return response;
-            }
-            return fetch(event.request);
-        })
+      caches
+        .match(event.request) // check if the request has already been cached
+        .then((cached) => cached || fetch(event.request)) // otherwise request network
+        .then(
+            (response) =>
+              cache(event.request, response) // put response in cache
+                .then(() => response) // resolve promise with the network response
+          )
     );
-});
+  });
 
 self.addEventListener("activate", (event) => {
     console.log("Service Worker activated.");
