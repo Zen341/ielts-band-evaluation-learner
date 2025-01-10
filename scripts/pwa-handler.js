8aa970eee479b2599e431e1c19700a9957c98521
Nghia Tran <119276144+nghiatm341@users.noreply.github.com>
2025-01-10 11:31:57 +0700

    new pwa service worker

--- scripts/pwa-handler.js
+++ scripts/pwa-handler.js
@@ -4,7 +4,8 @@ const serviceWorker = navigator.serviceWorker;
 // Register our ServiceWorker script, if serviceWorker is available.
 if (serviceWorker) {
     serviceWorker
-        .register("/scripts/service-worker.js")
+        .register("/pwabuilder-sw.js")
+        // .register("/service-worker.js")
         .then(() => console.log("ServiceWorker Registered to the Application!"))
         .catch(() => console.log("Failed to Register the ServiceWorker."));
 }
@@ -15,25 +16,25 @@ let beforeInstallEvent;
 // Reference the install button from DOM.
 const installButton = document.getElementById("btn-download");
 
-if (installButton) {
-    // Watch for the beforeinstallevent and defer it.
-    window.addEventListener("beforeinstallprompt", (event) => {
-        event.preventDefault();
-        beforeInstallEvent = event;
-        // installButton.style.display = "block";
-        installButton.classList.remove("d-none")
-    });
-    
-    // Prompt for Installation when install button is clicked.
-    installButton.addEventListener("click", () => {
-        beforeInstallEvent
-            .prompt()
-            .then((choice) => {
-                // Hide the install button as its purpose is over.
-                if (choice.outcome == "accepted") {
-                    // installButton.style.display = "none";
-                    installButton.classList.add("d-none")
-                }
-            });
-    });
-}
+
+// Watch for the beforeinstallevent and defer it.
+window.addEventListener("beforeinstallprompt", (event) => {
+    event.preventDefault();
+    beforeInstallEvent = event;
+    // installButton.style.display = "block";
+    installButton.classList.remove("d-none")
+});
+
+// Prompt for Installation when install button is clicked.
+installButton.addEventListener("click", () => {
+    beforeInstallEvent
+        .prompt()
+        .then((choice) => {
+            // Hide the install button as its purpose is over.
+            if (choice.outcome == "accepted") {
+                // installButton.style.display = "none";
+                installButton.classList.add("d-none")
+            }
+        });
+});
+
--- service-worker.js
+++ service-worker.js
@@ -6,10 +6,10 @@ let urlsToCache = [
     "/index.html",
     "/exam.html",
     "/settings.html",
-    "/images/icon-48x48.png", 
-    "/scripts/common.js", 
-    "/scripts/data.js", 
-    "/scripts/theme.js", 
+    "/images/icon-48x48.png",
+    "/scripts/common.js",
+    "/scripts/data.js",
+    "/scripts/theme.js",
     "/scripts/pwa-handler.js",
     "/bootstrap-5.3.3-dist/css/bootstrap.min.css",
     "/bootstrap-5.3.3-dist/js/bootstrap.min.js"
@@ -28,22 +28,22 @@ self.addEventListener("install", (event) => {
     );
 });
 
+self.addEventListener("activate", (event) => {
+    console.log("Service Worker activated.");
+});
+
 // Whenever a resource is requested, return if its cached else fetch the resource from server.
 self.addEventListener("fetch", (event) => {
     // Cache-First Strategy
     console.log("fetch event")
     event.respondWith(
-      caches
-        .match(event.request) // check if the request has already been cached
-        .then((cached) => cached || fetch(event.request)) // otherwise request network
-        .then(
-            (response) =>
-              cache(event.request, response) // put response in cache
-                .then(() => response) // resolve promise with the network response
-          )
+        caches
+            .match(event.request) // check if the request has already been cached
+            .then((cached) => cached || fetch(event.request)) // otherwise request network
+            .then(
+                (response) =>
+                    cache(event.request, response) // put response in cache
+                        .then(() => response) // resolve promise with the network response
+            )
     );
-  });
-
-self.addEventListener("activate", (event) => {
-    console.log("Service Worker activated.");
 });
